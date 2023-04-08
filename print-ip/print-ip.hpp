041a6c8e64de5eb0825b378d9ef5c3eb57ee60e7
4eam3less <4eam3less@gmail.com>
2023-04-08 23:25:06 +0200

    description updated

--- print-ip/print-ip.hpp
+++ print-ip/print-ip.hpp
@@ -1,11 +1,30 @@
 #pragma once
-
 #include <iostream>
 #include "additional_type_traits.hpp"
 
-/// test func
-/// \tparam Integer
-/// \param ip
+/// \mainpage   Print IP-address for C++
+///
+/// \brief      Print IP-address from various data.
+/// \author     Nameless
+/// \details
+/// Prints an IP address of various data types to the standard input output stream.\n
+/// \n
+/// Exsample: \n
+/// \n
+/// print_ip( int8_t{-1} ); // 255 \n
+/// print_ip( int16_t{0} ); // 0.0 \n
+/// print_ip( int32_t{2130706433} ); // 127.0.0.1 \n
+/// print_ip( int64_t{8875824491850138409} );// 123.45.67.89.101.112.131.41 \n
+/// print_ip( std::string{“Hello, World!”} ); // Hello, World! \n
+/// print_ip( std::vector<int>{100, 200, 300, 400} ); // 100.200.300.400 \n
+/// print_ip( std::list<shot>{400, 300, 200, 100} ); // 400.300.200.100 \n
+/// print_ip( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0 \n
+///
+
+
+/// \brief Prints an IP-address for integer type
+/// \tparam Integer integer type
+/// \param ip The integer type reference
 template<typename Integer,  std::enable_if_t<std::is_integral<Integer>::value, bool > = true >
 void print_ip(const Integer &ip){
     auto *start = reinterpret_cast<const unsigned char*> (&ip);
@@ -19,8 +38,11 @@ void print_ip(const Integer &ip){
     std::cout << std::endl;
 }
 
-template<typename T, std::enable_if_t<is_container<T>::value, bool> = true >
-void print_ip(const T &ip){
+/// \brief Prints an IP-address for container
+/// \tparam Container container type
+/// \param ip The container type reference
+template<typename Container, std::enable_if_t<is_container<T>::value, bool> = true >
+void print_ip(const Container &ip){
     for(auto it = ip.cbegin(); it != ip.cend(); ++it) {
         if (it != ip.cbegin())
             std::cout << ".";
@@ -29,11 +51,15 @@ void print_ip(const T &ip){
     std:: cout << std::endl;
 }
 
+/// \brief Prints a raw std string
+/// \tparam String std string
+/// \param ip The string type reference
 template<typename String, std::enable_if_t<std::is_same_v<String, std::basic_string<char>>, bool> = true>
 void print_ip(const String &ip){
     std::cout << ip << std::endl;
 }
 
+/// \brief This is an internal function for: void print_ip(const std::tuple<Args...> ip)
 template <typename TupleT, std::size_t... Is>
 void print_elements(const TupleT& tp, const std::index_sequence<Is...>) {
     size_t index = 0;
@@ -47,6 +73,9 @@ void print_elements(const TupleT& tp, const std::index_sequence<Is...>) {
     (print_element(std::get<Is>(tp)), ...);
 }
 
+/// \brief Prints an an IP-address for std::typle. \n std::typle types must be the same.
+/// \tparam Args args of the same type
+/// \param ip The std::typle
 template<typename ... Args, std::enable_if_t<is_one_type<Args...>::value, bool> = true >
 void print_ip(const std::tuple<Args...> ip) {
     print_elements(ip, std::make_index_sequence<std::tuple_size<decltype(ip)>::value>{});
