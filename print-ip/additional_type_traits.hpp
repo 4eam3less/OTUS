f683281e412824549904a1131dc71ae118ab9b5d
4eam3less <4eam3less@gmail.com>
2023-04-14 21:55:31 +0200

    simplification of verification is_one_type

--- print-ip/additional_type_traits.hpp
+++ print-ip/additional_type_traits.hpp
@@ -4,26 +4,31 @@
 #include <string>
 
 
-template <typename T, typename = void>
-struct is_container : std::false_type { };
+template<typename T, typename = void>
+struct is_container : std::false_type {
+};
 
-template <>
-struct is_container<std::basic_string<char>> : std::false_type { };
+template<>
+struct is_container<std::basic_string<char>> : std::false_type {
+};
 
-template <typename T>
+template<typename T>
 struct is_container<T,
-        std::void_t<decltype(std::declval<T&>().begin()),
-        decltype(std::declval<T&>().end()),
-        typename T::value_type >> : std::true_type { };
+        std::void_t<decltype(std::declval<T &>().begin()),
+                decltype(std::declval<T &>().end()),
+                typename T::value_type >> : std::true_type {
+};
 
 
 template<typename T, typename ... Args>
-struct is_one_type : std::true_type { };
+struct is_one_type : std::true_type {
+};
 
 template<typename T>
-struct is_one_type<T> : std::true_type { };
+struct is_one_type<T> : std::true_type {
+};
 
 template<typename T, typename U, typename ...Args>
-struct is_one_type<T, U, Args...>{
-    static constexpr auto value = !std::is_same_v<T, U> ? false : is_one_type<U, Args...>::value;
+struct is_one_type<T, U, Args...> {
+    static constexpr auto value = std::is_same_v<T, U> && is_one_type<U, Args...>::value;
 };
